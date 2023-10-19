266417b364c981ad2a79b3ff696867d2d9ce9a32
Ryan Agung Samudra <ryansamudra67@gmail.com>
2023-10-19 16:51:30 +0700

    first init

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-[100vh] w-full flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
